5d069256f0fb8637df42c2d6853b80069e2cc2ac
Kush <kush@lavios-arch.localdomain>
2018-01-24 17:54:35 +0530

    Fix: CSS getting decoded to HTML Entities

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,51 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+const FormCss = `
+    #mc-embedded-subscribe-form {
+        font-family: RobotoMono, sans-serif;
+        width: 100%;
+        padding-top: 10px;
+        text-align: center;
+    }
+
+    div.mc-field-group {
+        display: inline-block;
+        width: 70%;
+    }
+
+    div.mc-field-group > input {
+        width: 100%;
+    }
+
+    .clear {
+        display: inline-block;
+    }
+
+    div.mc-field-group > input, input.button {
+        padding: 8px;
+        color: black;
+        background-color: white;
+        border: 1px solid black;
+    }
+
+    input.button {
+        width: 100%;
+        color: white;
+        padding: 8px 20px;
+        background-color: #2d4191;
+        cursor: pointer;
+    }
+
+    @media (max-width: 500px) {
+        div.mc-field-group > input, input.button {
+            font-size: 14px;
+            padding: 6px 10px;
+        }
+    }
+`;
+
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
@@ -14,52 +59,7 @@ const TemplateWrapper = ({ children }) => (
         { name: 'apple-mobile-web-app-status-bar-style', content: "#2d4191" }
       ]}
     />
-    <style type="text/css">
-      {`
-          #mc-embedded-subscribe-form {
-              font-family: RobotoMono, sans-serif;
-              width: 100%;
-              padding-top: 10px;
-              text-align: center;
-          }
-
-          div.mc-field-group {
-            display: inline-block;
-            width: 70%;
-          }
-
-          div.mc-field-group > input {
-            width: 100%;
-          }
-
-          .clear {
-            display: inline-block;
-          }
-
-          div.mc-field-group > input, input.button {
-            padding: 8px;
-            color: black;
-            background-color: white;
-            border: 1px solid black;
-          }
-
-          input.button {
-            width: 100%;
-            color: white;
-            padding: 8px 20px;
-            background-color: #2d4191;
-            cursor: pointer;
-          }
-
-          @media (max-width: 500px) {
-            div.mc-field-group > input, input.button {
-                font-size: 14px;
-                padding: 6px 10px;
-            }
-          }
-
-      `}
-    </style>
+    <style type="text/css" dangerouslySetInnerHTML={{ __html: FormCss }} />
     <div>
       {children()}
     </div>
